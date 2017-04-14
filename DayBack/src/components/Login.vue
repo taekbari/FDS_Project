7edbb51924397ed1289d41b0d36ef7f7f827e22b
Jennybe <jennybe0117@gmail.com>
2017-04-14 16:15:00 +0900

    Added Login component
    로그인 컴포넌트를 메인 루트 프로젝트에 추가

---
+++ DayBack/src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login">
+    <!--로그인 모달 창을 띄우고 없애는 modal 처리 컴포넌트.-->
+    <button 
+      class="button is-danger is-outlined"
+      id="show-modal"
+      @click="showModal = true">로그인 화면 띄우기
+    </button>
+    <modal
+      class="modal-template" 
+      v-if="showModal"
+      @closeModal="showModal = false"
+    ></modal>
+  </div>
+</template>
+
+<script>
+import Modal from './Modal.vue';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello Vue, I just started :)',
+      showModal: false,
+      flipModal: false
+    }
+  },
+  components: {
+    Modal
+}
+}
+</script>
+
+<style lang="sass">
+
+.login
+  margin: 1rem
+
+</style>
---
+++ DayBack/src/components/Modal.vue
@@ -0,0 +1,347 @@
+<template>
+  <div class="modal-template">
+    <transition
+      name="flipInY"
+      enter-active-class="animated flipInY"
+      leave-active-class="animated flipOutY"
+      mode="out-in"
+      :duration="{leave: 500}"
+      appear 
+     >
+      <div v-if="!show" class="wrapper signbox">
+        <div class="container">
+          <a
+            @click="closeModal"
+            class="close-button">x</a>
+          <h2 class="title is-2">{{signin.title}}</h2>
+          <!--input form으로 뺄 수 있는 부분-->
+            <form mothod="post" class="form-signin">
+              <fieldset>
+                <input class="login" required type="text" name="email"
+                v-model="signin.email"
+                id="email" placeholder="이메일">
+                <label for="login-id"></label>
+                <input class="login" required type="password" name="password" 
+                v-model="signin.password"
+                id="password" placeholder="비밀번호">
+                <label for="login-id"></label>
+                <input 
+                  @click="logIn" required
+                  type="submit" value="로그인" class="submit-button">
+                <label for="login button"></label>
+                <a href="#"
+                  @click="flipModal"
+                  class="signup-button">{{signin.signup}}
+                </a>
+              </fieldset>
+            </form>
+            <div class="social">
+              <p class="login-info">{{signin.loginInfo}}</p>
+              <div class="social-buttons">
+                <a href="#" class="social-google"
+                @click=""
+                ><span>G</span></a>
+                <a href="#" class="social-facebook"
+                @click=""
+                ><span>f</span></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      <div v-else class="wrapper-signup signbox">
+        <div class="container">
+          <a
+            @click="closeModal"
+            class="close-button">x</a>
+          <h2 class="title is-2">{{signup.title}}</h2>
+          <form mothod="post" class="form-signin">
+            <fieldset>
+              <input class="login" type="textarea" name="username"
+                v-model="signup.nickname" maxlength="10"
+                id="username" placeholder="닉네임">
+              <label for="login-id"></label>
+              <input class="login" type="text" name="email" 
+                v-model="signup.email"
+                id="email" placeholder="이메일">
+              <label for="login-id"></label>
+              <input class="login" type="password" name="password" 
+               v-model="signup.password"
+               id="password" placeholder="비밀번호">
+              <label for="login-id"></label>
+              <input class="login" type="password" name="password" 
+               v-model="signup.passwordCheck"
+               id="passwordCheck" placeholder="비밀번호 재확인">
+              <label for="login-id"></label>
+              <input 
+                @click="signUp"
+                type="submit" value="가입하기" class="signup-button">
+              <label for="signup button"></label>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import Box from './Box.vue';
+import axios from 'axios';
+
+export default {
+  data () {
+    return {
+      show: false,
+      signin: {
+        title: "로그인",
+        signup: "회원가입",
+        loginInfo: "또는 아래의 계정으로 로그인",
+        email: '',
+        password: '',
+      },
+      signup: {
+          title: "회원가입",
+          signup: "입력완료",
+          loginInfo: "환영합니다",
+          nickname: '',
+          email: '',
+          password: '',
+          passwordCheck: ''
+      }
+    }
+  },
+  components: {
+    Box
+  },
+  methods: {
+    closeModal: function() {
+      this.$emit('closeModal');
+    },
+    flipModal: function() {
+      this.show = !this.show;
+    },
+    signUp: function(e) {
+      // 기본 이벤트(submit) 막음
+      e.preventDefault();
+      var _this = this;
+      axios.post('https://dayback.hcatpr.com/signup/', {
+      // axios.post('http://localhost:3000/users', {
+        // 화면에서 넣는 data 형식 POST 하기
+        email: this.signup.email,
+        password: this.signup.password,
+        nickname: this.signup.nickname
+      })
+      .then(response => {
+        // 서버 통신 성공시
+        console.log(response)
+        console.log("성공")
+        window.alert('가입에 성공했습니다. 이제 로그인 해보세요! :-)');
+        // 가입하면 다시 로그인 페이지로 돌아가기
+        _this.flipModal();
+      })
+      .catch((error) => {
+        window.alert('가입한 내용을 다시 한 번 확인해 주세요.');
+        console.log(error)
+      })
+    },
+    logIn: function(e) {
+      e.preventDefault();
+      // this.$router.push('/service');
+      axios.post('https://dayback.hcatpr.com/login/', {
+        // results: {
+          email: this.signin.email,
+          password: this.signin.password
+        // }
+      })
+      .then(response => {
+          var _this = response.data;
+          console.log('_this', _this);
+          console.log('_this.key', _this.key);
+          console.log(response.status);
+        // 사용자 정보를 가져온다
+          //   axios.get('https://dayback.hcatpr.com/user/ '+ _this.key, {
+          //     results: {
+          //       nickname: this.nickname,
+          //       email: this.email,
+          //       password: this.password
+          //     }
+          //   })
+          //   .then(response => {
+          //   console.log(response)
+          //   console.log("성공")
+          //   window.alert(response._this.key.user.results.nickname+ '님이 로그인 하셨습니다.');
+          // })
+
+        window.alert(response.key+ '님이 로그인 하셨습니다.');
+        this.$router.push({path: './service'})
+      })
+      .catch((error) => {
+        console.log(error)
+        window.alert('이메일 주소나 비밀번호에 문제가 있어요!');
+      })
+    }
+  },
+}
+
+</script>
+
+<style lang="sass">
+.modal-template
+  position: fixed
+  top: 0
+  left: 0
+  width: 100%
+  height: 100%
+  background: #3f3f3f
+  z-index: 10
+  transition: opacity 1s ease
+
+// _common-modules.sass
+a, button
+ text-decoration: none
+
+%flex-col-center
+  display: flex
+  flex-flow: column
+  justify-content: center
+  align-items: center
+
+%button-hover-scale
+  cursor: pointer
+  width: 300px
+  transform: scale(1.12) 1s ease
+  transition: all 0.3s ease-in-out
+
+fieldset
+  border: 0
+  @extend %flex-col-center
+
+// local styling
+.wrapper, .wrapper-signup
+  margin: 4rem auto
+  display: flex
+  @extend %flex-col-center
+  &-signup
+    position: relative
+    top: 0
+
+.container
+  position: relative
+  width: 400px
+  height: 500px
+  background-color: #fff
+  border: 1px solid #a322ef
+  border-radius: 3px
+  z-index: 100
+  @extend %flex-col-center
+  
+.close-button
+  position: absolute
+  top: -10px
+  right: -30px
+  font-size: 30px
+  color: #fff
+
+.title
+  margin-bottom: 30px
+  color: #a322ef
+  font-weight: 400
+
+.form-signin
+  line-height: 30px
+  font-size: 17px
+  width: 260px
+  @extend %flex-col-center
+  .login
+    border: 0
+    border-bottom: 5px solid #dcdcdc
+    padding: 5px 0
+    margin: 0 auto 2px auto
+    font-size: 25px
+    text-indent: 10px 
+  .submit-button, .signup-button
+    width: 260px
+    border: 1px solid #C5C1FF
+    border-radius: 20px
+    height: 40px
+    line-height: 40px
+    font-size: 17px
+    text-align: center
+    background: #fff
+    box-sizing: border-box
+    margin: 10px auto 15px auto
+  .submit-button
+   color: #b145f0
+   margin-top: 30px
+   &:hover
+    @extend %button-hover-scale
+  .signup-button
+    border: none
+    background-image: linear-gradient(65deg, #FFBAC3 0%, #C5C1FF 56%, #2CD8D5 100%)
+    color: #fff
+    margin-bottom: 30px
+    &:hover
+     @extend %button-hover-scale
+
+.social
+  display: flex
+  flex-flow: column
+  justify-content: center
+  width: 300px
+  .login-info
+    color: #505050
+    font-size: 12px
+    letter-spacing: -1px
+    display: block
+    text-align: center
+    position: relative
+    font-size: 0.9rem
+    &::before,
+    &::after
+      content: ''
+      position: absolute
+      top: 50%
+      height: 1px
+      background: #a0a0a0
+      width: 20%
+    &::before
+      left: 10px
+    &::after
+      right: 10px
+
+  .social-buttons
+    width: 300px
+    height: 50px
+    display: flex
+    flex-flow: row
+    justify-content: space-around
+    margin-top: 10px
+    .social-google, .social-facebook
+      align-items: center
+      position: relative
+      width: 50px
+      height: 50px
+      border-radius: 50%
+    .social-google
+      background-color: rgb(82.3%, 32.1%, 32.1%)
+      margin-left: 60px
+      span
+        position: absolute
+        font-family: Avenir
+        top: 17px
+        left: 13px
+        font-size: 30px
+        color: #fff
+        font-weight: 600
+    .social-facebook
+      background-color: rgb(19.5%, 49.2%, 92.1%)
+      margin-right: 60px
+      span
+        position: absolute
+        font-family: Avenir
+        top: 18px
+        left: 19px
+        font-size: 30px
+        color: #fff
+        font-weight: 600
+</style>
