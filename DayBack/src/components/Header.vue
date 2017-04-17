04e6ca0631f6a5e4a054af00cdeb4ee1e2b43785
김택현 <taekbari157@gmail.com>
2017-04-17 16:41:41 +0900

    Add header, video components and resources
    Header and video components that make up the main page have been
    added.

---
+++ DayBack/src/components/Header.vue
@@ -0,0 +1,87 @@
+<template lang="html">
+  <header class="fixed-header">
+    <div class="fix-width">
+      <h1 class="logo-heading">
+        <a href="" class="logo-heading__link">
+          DayBack
+        </a>
+      </h1>
+      <nav class="gnb">
+        <h2>Global Navigation</h2>
+        <ul class="row-list">
+          <li><a class="gnb__item" href="">About</a></li>
+          <li><a class="gnb__item" href="">Toturial</a></li>
+          <li><a class="gnb__item" href="">Member</a></li>
+          <li><a class="gnb__item" href="" @click.prevent="showSignInPopup">SignIn</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  methods: {
+    showSignInPopup() {
+      this.$eventBus.$emit('showSignInPopup');
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+ul
+  padding: 0
+  margin: 0
+  list-style: none
+
+.fixed-header
+  width: 100%
+  background: rgba(3, 3, 3, 0.7)
+  position: fixed
+  top: 0
+  left: 0
+  z-index: 100
+
+  & a
+    color: #fff
+
+.fix-width
+  width: 960px
+  padding: 1.25rem 0
+  margin: 0 auto
+
+  &::after
+    display: block
+    content: ''
+    clear: both
+
+.logo-heading
+  float: left
+  margin: 0
+  font-size: 1.5rem
+  font-weight: 400
+  height: 30px
+
+.gnb
+  float: right
+
+  &__item
+    padding: 5px 8px
+    margin: 0 5px
+    border: 1px solid transparent
+
+    &:hover,
+    &:focus
+      border: 1px solid #fff
+
+  & h2
+    position: absolute
+    left: -99999em
+
+.row-list
+  display: flex
+  height: 30px
+  justify-content: center
+  align-items: center
+</style>
---
+++ DayBack/src/components/Video.vue
@@ -0,0 +1,131 @@
+<template lang="html">
+  <div class="homepage-hero-module">
+    <div class="video-container">
+      <video autoplay loop class="fillWidth">
+        <source :src="videoSrcMP4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
+        <source :src="videoSrcWebm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
+      </video>
+      <div class="poster hidden">
+        <img src="../assets/video/pencil_down.jpg" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    let _this = this;
+    this.scaleVideoContainer();
+
+    this.initBannerVideoSize('.video-container .poster img');
+    this.initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+      console.log('call resize');
+        _this.scaleVideoContainer();
+        _this.scaleBannerVideoSize('.video-container .poster img');
+        _this.scaleBannerVideoSize('.video-container video');
+    });
+
+    $(document).keyup(function(e) {
+      if (e.keyCode === 27) {
+        let video = document.querySelector('.video-container video');
+        if (video.paused) {
+          video.play();
+        }
+        else {
+          video.pause();
+        }
+      }
+    });
+  },
+  methods: {
+    scaleVideoContainer() {
+      let height = $(window).height() + 5;
+      let unitHeight = parseInt(height) + 'px';
+      $('.homepage-hero-module').css('height',unitHeight);
+    },
+    initBannerVideoSize(element) {
+      $(element).each(function(){
+          $(this).data('height', $(this).height());
+          $(this).data('width', $(this).width());
+      });
+
+      this.scaleBannerVideoSize(element);
+    },
+    scaleBannerVideoSize(element) {
+      let windowWidth = $(window).width(),
+      windowHeight = $(window).height() + 5,
+      videoWidth,
+      videoHeight;
+
+      $(element).each(function(){
+          let videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+          $(this).width(windowWidth);
+
+          if(windowWidth < 1000){
+              videoHeight = windowHeight;
+              videoWidth = videoHeight / videoAspectRatio;
+              $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px', 'max-width': videoWidth});
+
+              $(this).width(videoWidth).height(videoHeight);
+          }
+
+          $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+      });
+    }
+  },
+  computed: {
+    videoSrcMP4() {
+      return require('../assets/video/pencil_down.mp4');
+    },
+    videoSrcWebm() {
+      return require('../assets/video/pencil_down.webm');
+    }
+  }
+}
+</script>
+
+<style lang="css">
+.homepage-hero-module {
+    border-right: none;
+    border-left: none;
+    position: relative;
+}
+.no-video .video-container video,
+.touch .video-container video {
+    display: none;
+}
+.no-video .video-container .poster,
+.touch .video-container .poster {
+    display: block;
+}
+.video-container {
+    position: relative;
+    bottom: 0%;
+    left: 0%;
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    background: #000;
+}
+.video-container .poster img {
+    width: 100%;
+    bottom: 0;
+    position: absolute;
+}
+.video-container .poster.hidden {
+  display: none;
+}
+.video-container video {
+    position: absolute;
+    z-index: 0;
+    bottom: 0;
+}
+.video-container video.fillWidth {
+    width: 100%;
+}
+</style>
