20cd92bde25dbb2a42a17eb99bf4f218d6cc67ed
Jennybe <jennybe0117@gmail.com>
2017-04-18 19:32:51 +0900

    Added vue State Management object store state for deliver token key value from login to service page
    Vue 상태 관리를 위한 오브젝트인 store state를 main.js에 전역으로 추가하여 로그인 시 각 유저가 받는 고유한 토큰 값을 서비스 페이지로도 전달할 수 있도록 함

--- DayBack/src/main.js
+++ DayBack/src/main.js
@@ -2,16 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 
 const eventBus = new Vue();
+const store = {
+    debug: true,
+    state: {
+        key: ''
+    }
+}
 
 Object.defineProperties(Vue.prototype, {
-  $eventBus: {
-    get() {
-      return eventBus;
+    $eventBus: {
+        get() {
+            return eventBus;
+        }
+    },
+    $store: {
+        get: function() {
+            return store;
+        }
     }
-  }
 });
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
